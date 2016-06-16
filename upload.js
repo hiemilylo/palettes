745fce5b56380fdce819cc5b8fbf0799e18bfcfa
Emily Lo <hiemilylo@gmail.com>
2016-06-16 05:40:06 -0700

    Two canvases, aligns depending on portrait or landscape

--- upload.js
+++ upload.js
@@ -1,7 +1,9 @@
 function readURL(input) {
        if (input.files && input.files[0]) {
+           var maxWidth = screen.width;
+           var maxHeight = screen.height;
            var reader = new FileReader();
-           var c = document.getElementById("myCanvas");
+           var c = document.getElementById("origCanvas");
            var ctx = c.getContext("2d");
            reader.onload = function (e) {
                    var img = document.createElement("IMG");
@@ -11,14 +13,28 @@ function readURL(input) {
                    while ( imgWidth > screen.width - 100 || imgHeight > screen.height - 100 ){
                       imgWidth = parseInt(imgWidth*9/10);
                       imgHeight = parseInt(imgHeight*9/10);
-                      console.log(imgWidth + " " + screen.width );
-                      console.log(imgHeight + " " + screen.height );
+                      console.log(imgWidth + " " + maxWidth );
+                      console.log(imgHeight + " " + maxHeight );
                    }
                    img.width = ""  + imgWidth;
                    img.height = "" + imgHeight;
                    c.width = ""  + imgWidth;
                    c.height = "" + imgHeight;
                    ctx.drawImage(img, 0, 0, imgWidth, imgHeight);
+
+                   var c2 = document.getElementById("newCanvas");
+                   c2.width = ""  + imgWidth;
+                   c2.height = "" + imgHeight;
+                   var ctx2 = c2.getContext("2d");
+
+                   if ( imgHeight > imgWidth ){
+                      c2.style.display = 'inline';
+                   }
+                   else{
+                      c2.style.display = 'block';
+                   }
+
+                   ctx2.drawImage(img, 0, 0, imgWidth, imgHeight);
            };
            reader.readAsDataURL(input.files[0]);
        }
